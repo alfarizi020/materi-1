a513f2c3e7afe314c3c2be268f37586f4183da15
N2YM <akmalalfarizi1231@gmail.com>
2022-07-24 20:24:58 +0700

    Belajar Inheritence

--- index.php
+++ index.php
@@ -10,24 +10,31 @@ class mobil {
     }
 }
 
-$bmw = new Mobil;
+
+
+ class BMW  extends Mobil {
+    
+ }
+
+ class Tesla extends Mobil {
+    function selfParking(){
+        echo"Parkir sendiri";
+    }
+ }
+
+$bmw = new BMW;
 $bmw->merk = "BMW";
 $bmw->tipe = "320i";
 $bmw->mesin = "2000cc";
 $bmw->max_speed = "280km/h";
 
-$bmw2 = new mobil;
-$bmw2->merk = "BMW";
-$bmw2->tipe = "320i";
-$bmw2->mesin = "2000cc";
-$bmw2->max_speed = "280km/h";
+$tesla = new  Tesla ;
+$tesla->merk = "Tesla";
+$tesla->tipe = " Model X";
+$tesla->mesin = "Listrik";
+$tesla->max_speed = "280km/h";
 
-echo "$bmw2->tipe";
-echo "<br>";
-echo "$bmw->tipe";
 
-if($bmw === $bmw2){
-    echo "Sama";
-}else{
-    echo "Tidak";
-}
+
+
+echo $tesla->kecepatanMaksimal();
